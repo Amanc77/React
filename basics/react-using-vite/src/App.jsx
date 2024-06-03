1e200f914f87b3d2eb916bb456f3dc856b13da46
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-06-03 23:13:08 +0530

    r

---
+++ basics/react-using-vite/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <dev>
+         <h1>I am learing React with vite</h1>
+
+         <h6>how are you </h6>
+
+         <h1>i am fine and what about you </h1>
+         
+    </dev>
+
+
+   
+  )
+}
+
+export default App
